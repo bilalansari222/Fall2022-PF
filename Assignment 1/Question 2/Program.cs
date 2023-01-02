157c5fba3fa00514098d2fdd6dd1582d4f1f85c8
bilalansari222 <118686647+bilalansari222@users.noreply.github.com>
2023-01-02 14:28:22 +0500

    Add files via upload

---
+++ Assignment 1/Question 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter a number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine(" The given number is odd.");
+            }
+            else
+            {
+                Console.WriteLine(" The given number is even.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
