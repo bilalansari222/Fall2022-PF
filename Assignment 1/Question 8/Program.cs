3d1c0b1cf2711cafec28fd361e1754c9bbb4d11e
bilalansari222 <118686647+bilalansari222@users.noreply.github.com>
2023-01-02 15:30:36 +0500

    Add files via upload

---
+++ Assignment 1/Question 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numbers Of Rows:");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int a, b;
+            for (a = 1; a <= r; a++)
+            {
+                for (b = 1; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
